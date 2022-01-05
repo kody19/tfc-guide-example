503093830b1697c687f632201de0a0341f24baad
kody19 <41171697+kody19@users.noreply.github.com>
2022-01-04 21:31:58 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,35 +2,7 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_default_vpc" "default" {
-  tags = {
-    Name = "Default VPC"
-  }
+
+resource "aws_vpc" "myvpc" {
+  cidr_block = "10.0.0.0/18"
 }
-
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = var.instance_name
-  }
-}
-
